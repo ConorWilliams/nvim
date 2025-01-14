0f954000c4364a8279f6a7456d6ebb3b0769ebba
Conor <conor@myrtle.com>
2025-01-14 17:57:48 +0000

    zk added

---
+++ lua/plugins/zk.lua
@@ -0,0 +1,24 @@
+return {
+  "zk-org/zk-nvim",
+
+  keys = {
+    -- New note in same dir as current buffer.
+    {
+      "<leader>fz",
+      "<Cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>",
+      desc = "New note",
+    },
+    -- Find note
+    {
+      "<leader>z",
+      "<Cmd>ZkNotes<CR>",
+      desc = "Find Note",
+    },
+  },
+
+  config = function()
+    require("zk").setup({
+      picker = "fzf_lua",
+    })
+  end,
+}
