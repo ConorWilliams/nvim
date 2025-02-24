c50babb3bb10cf0f10c94ccee0d229365569f568
Conor <conor@myrtle.com>
2025-02-24 09:21:36 +0000

    disable render latex from render.markdown

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,9 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    latex = {
+      -- Whether LaTeX should be rendered, mainly used for health check
+      enabled = false,
+    },
+  },
+}
